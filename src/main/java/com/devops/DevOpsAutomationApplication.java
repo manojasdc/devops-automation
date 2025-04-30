5c01415228da0640e7a32d1e1d61e582d20265cc
manoj <manojrajoriya1995gmail.com>
2025-04-30 16:11:50 +0530

    first commit

---
+++ src/main/java/com/devops/DevOpsAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsAutomationApplication.class, args);
+	}
+
+}
